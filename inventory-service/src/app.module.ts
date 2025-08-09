30bd0ef7e3b9ba95ce7a37b16e8f3497572168ca
denilsondev <denilsoncdc10@gmail.com>
2025-08-09 16:05:42 -0300

    ajuste

--- inventory-service/src/app.module.ts
+++ inventory-service/src/app.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
-import { PrismaClient } from '../generated/prisma';
-
-const prisma = new PrismaClient();
 
 @Module({
   imports: [],
   controllers: [AppController, HealthController],
-  providers: [AppService, { provide: PrismaClient, useValue: prisma }],
+  providers: [AppService],
 })
 export class AppModule {}
